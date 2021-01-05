2c8e8397ab265deae29e237253c69ce365c1e92c
Lucas <lucasdelellis2002@gmail.com>
2021-01-05 10:18:50 -0300

    #4 - Rethinking Movement

--- snake.py
+++ snake.py
@@ -1,6 +1,7 @@
 import pygame
 from sys import exit
 from math import floor
+from random import randrange
 
 # Functions ------------------------------------
 def movement():
@@ -30,17 +31,19 @@ pygame.display.set_caption("Snake By @Lucazz82")
 
 # Grid Setup
 menuHeight = 100
+squareSize = 20
+maxColumns = int(size[0] / squareSize)
+maxRows = int((size[1] -menuHeight) / squareSize)
 
 # Snake Setup
-squareSize = 20
 speed = squareSize
 
 class SnakeBody():
-    def __init__(self, row, column, index):
+    def __init__(self, row, column, index, direction):
         self.rect = pygame.Rect(column * squareSize, (row * squareSize) + menuHeight, squareSize, squareSize)
         self.row = row
         self.column = column
-        self.direction = "right"
+        self.direction = direction
         self.index = index
         self.nextDirection = []
 
@@ -55,7 +58,7 @@ class SnakeBody():
 
 class SnakeHead(SnakeBody):
     def __init__(self, row, column, index):
-        SnakeBody.__init__(self, row, column, index)
+        SnakeBody.__init__(self, row, column, index, "")
 
     def changeDirection(self, direction):
         if direction in ["up", "down"] and self.direction in ["up", "down"]:
@@ -67,16 +70,50 @@ class SnakeHead(SnakeBody):
         self.direction = direction
 
         pos = [self.row, self.column, direction]
-        for square in snake.body:
+        for square in snake.body[1:]:
             square.nextDirection.append(pos)
+
+class Berry():
+    def __init__(self):
+        self.column = randrange(maxColumns)
+        self.row = randrange(maxRows)
+        self.rect = pygame.Rect(self.column * squareSize, (self.row * squareSize) + menuHeight, squareSize, squareSize)
+        self.createBody = False
+
+    def checkColision(self):
+        if self.rect.colliderect(snake.body[0]):
+            self.move()
+            self.createBody = True
+    
+    # Move to a place there isnt a snake
+    def move(self):
+        print("Culo")
+        self.column = randrange(maxColumns)
+        print(self.column)
+        self.row = randrange(maxRows)
+        print(self.row)
+        self.rect.x = self.column * squareSize
+        self.rect.y = self.row * squareSize + 100
+        # self.rect = self.rect.move(self.column * squareSize, (self.row * squareSize) + menuHeight)
+        snake.nextBody.append([snake.body[-1].row, snake.body[-1].column, snake.body[-1].direction])
 
 class Snake():
     def __init__(self):
         self.body = []
+        self.nextBody = []
+
+    def addBody(self):
+        index = len(self.body) - 1
+        # self.body.append(SnakeBody(self.body[index].row, self.body[index].column, len(self.body), self.body[index].direction))
+        self.body.append(SnakeBody(self.nextBody[0][0], self.nextBody[0][1], len(self.body), self.nextBody[0][2]))
+        self.nextBody.pop(0)
+        berry.createBody = False
+        pass
 
 # Game Setup
 snake = Snake()
 snake.body.append(SnakeHead(5, 5, len(snake.body)))
+berry = Berry()
 # snake.body.append(SnakeBody(5, 4, len(snake.body)))
 # snake.body.append(SnakeBody(5, 3, len(snake.body)))
 # snake.body.append(SnakeBody(5, 2, len(snake.body)))
@@ -104,31 +141,42 @@ while True:
             if event.key == pygame.K_RIGHT:
                 snake.body[0].changeDirection("right")
                 pass
+            if event.key == pygame.K_SPACE:
+                snake.addBody()
+                pass
 
     # Snake movement
     movement()
-
+    
+    # Change Direction
     for square in snake.body[1:]:
         square.changeDirection()
 
+    if berry.createBody:
+        snake.addBody()
 
-    
+    # Colision -------------------------------------
+    berry.checkColision()
+    # ----------------------------------------------
     
     # Visuals --------------------------------------
     # Background
     screen.fill((200,200,200))
 
     # Drawing The Grid
-    # for x in range(1, int(size[0] / squareSize)):
-    #     pygame.draw.line(screen, (0,0,0), (squareSize * x, menuHeight), (squareSize * x, size[1]), 1)
+    for x in range(1, int(size[0] / squareSize)):
+        pygame.draw.line(screen, (0,0,0), (squareSize * x, menuHeight), (squareSize * x, size[1]), 1)
 
-    # for y in range(int(size[1] / squareSize)):
-    #     pygame.draw.line(screen, (0,0,0), (0, menuHeight + y * squareSize), (size[0], menuHeight + y * squareSize), 1)
+    for y in range(int(size[1] / squareSize)):
+        pygame.draw.line(screen, (0,0,0), (0, menuHeight + y * squareSize), (size[0], menuHeight + y * squareSize), 1)
     
     # Drawing Snake
     for square in snake.body:
         pygame.draw.rect(screen, (124,252,0), square.rect)
     
+    # Drawing Berrys
+    pygame.draw.rect(screen, (255,0,0), berry.rect)
+
     #-----------------------------------------------
     
     # Update Screen
